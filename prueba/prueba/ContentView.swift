1d4900c4444d0f0f42947cacaf72e1ffadc35b8a
galigaribaldi <galigaribaldi@live.com>
2020-08-25 23:51:01 -0500

    Commit de Prueba

---
+++ prueba/prueba/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  prueba
+//
+//  Created by Hernán Galileo Cabrera Garibaldi on 25/08/20.
+//  Copyright © 2020 Hernán Galileo Cabrera Garibaldi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
